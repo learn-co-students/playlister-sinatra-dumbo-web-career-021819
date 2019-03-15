d56a3539c41099c39453f3970e38615fec83337d
1-8192 <allegranzia@gmail.com>
2019-03-14 22:24:29 -0400

    worked on forms

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,14 +10,32 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
-  post 'songs/' do
-    @song=Song.create(params)
-    redirect to :"/songs/#{@song.slug}"
+  post '/songs' do
+    @song=Song.create(name: params["Name"])
+    @song.artist=Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    params.delete("_method")
+    @song.update(params)
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
